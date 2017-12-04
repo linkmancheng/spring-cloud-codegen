815a5fd7528e257d5edb20b63c055870060ecd74
liumapp <liumapp.com@gmail.com>
2017-12-04 12:25:34 +0800

    solve es2016

--- spring-cloud-codegen-ui/src/main.js
+++ spring-cloud-codegen-ui/src/main.js
@@ -1,15 +1,56 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+//import './assets/theme/theme-green/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
+//import NProgress from 'nprogress'
+//import 'nprogress/nprogress.css'
+import routes from './routes'
+import Mock from './mock'
+Mock.bootstrap();
+import 'font-awesome/css/font-awesome.min.css'
 
-Vue.config.productionTip = false
+import N3Components from 'N3-components'
+import 'N3-components/dist/index.min.css'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+Vue.use(N3Components)
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+//NProgress.configure({ showSpinner: false });
+
+const router = new VueRouter({
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+  //NProgress.start();
+  if (to.path == '/login') {
+    sessionStorage.removeItem('user');
+  }
+  let user = JSON.parse(sessionStorage.getItem('user'));
+  if (!user && to.path != '/login') {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+//router.afterEach(transition => {
+//NProgress.done();
+//});
+
+new Vue({
+  //el: '#app',
+  //template: '<App/>',
+  router,
+  store,
+  //components: { App }
+  render: h => h(App)
+}).$mount('#app')
+
