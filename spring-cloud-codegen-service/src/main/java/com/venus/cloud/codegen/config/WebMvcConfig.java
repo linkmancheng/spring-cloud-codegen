456180e34dd129470b9bc820050b4fc7fe3284b5
lyl2008dsg <lyl2008dsg@163.com>
2017-12-06 09:28:54 +0800

    fixed the cors for backstage

---
+++ spring-cloud-codegen-service/src/main/java/com/venus/cloud/codegen/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.venus.cloud.codegen.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**").allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+
+    }
+}
